44261f316ebbd09dd97086b7aecb6d8107385346
julianrod94 <julrodriguez@itba.edu.ar>
2016-05-14 12:48:32 -0300

    refactor 3

--- Api/socket/socket.h
+++ Api/socket/socket.h
@@ -1,7 +1,6 @@
 #ifndef _SOCKET_H_
 #define _SOCKET_H_
 
-// From http://stackoverflow.com/questions/2784500/how-to-send-a-simple-string-between-two-programs-using-pipes
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
@@ -11,31 +10,5 @@
 #include <unistd.h>
 #include "../commons.h"
 
-#define NOT_FOUND_ERR 0
-
-#define REQUEST_QUEUE "giladita"
-
-#define BLOCK 1024 
-
-//the client should use this function to start a request
-//request is initialized and sent to the server
-Request * createRequest(int action, int fd, size_t dataSize, void * data);
-
-Connection * createConnection(int fd);
-
-/**
- *  Returns two fds for the namedPipeName
- *
- *  @param  namedPipeName name of the pipe
- */
-int * openNamedPipe(char * something);
-
-void writeNamedPipe(int fd, void * data, int size);
-
-int readNamedPipe (int fd, void * buffer);
-
-int closeNamedPipe(int fd, char * something);
-
-Request * getRequest(Connection * connection);
 
 #endif
