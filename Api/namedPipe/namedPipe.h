b7e6cee97a486acfbfb5c1d54a4d19793ca9b44a
Noelia Belen Lopez <nlopez@itba.edu.ar>
2016-05-16 17:52:06 -0300

    Read students checked (#33)
    * Cleaning the code
    
    * Read checked

--- Api/namedPipe/namedPipe.h
+++ Api/namedPipe/namedPipe.h
@@ -1,7 +1,6 @@
 #ifndef _NAMED_PIPE_H_
 #define _NAMED_PIPE_H_
 
-// From http://stackoverflow.com/questions/2784500/how-to-send-a-simple-string-between-two-programs-using-pipes
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
@@ -12,14 +11,9 @@
 #include "../commons.h"
 
 #define NOT_FOUND_ERR 0
-
 #define REQUEST_QUEUE "giladita"
-#define SQL_QUEUE "bandadegilada"
-
 #define BLOCK 1024 
 
-//the client should use this function to start a request
-//request is initialized and sent to the server
 Request * createRequest(int action, int fd, int dataSize, void * data);
 
 Connection * createConnection(int fd);
