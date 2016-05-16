36011de3a061c72389529480e09f1a2782c21445
julianrod94 <julrodriguez@itba.edu.ar>
2016-05-16 17:37:30 -0300

    emprolaishing

--- Api/socket/socket.h
+++ Api/socket/socket.h
@@ -10,6 +10,21 @@
 #include <unistd.h>
 #include "../commons.h"
 
+#define SOCKETPATH "/tmp/giladitaSocket"
+#define LISTEN_BACKLOG 50
+#define	MAX_CONNECTIONS 100
+
+
+static const char * serverMsg[5] = {
+  "Estudiante agregado con exito ",
+  "El estudiante ya existe ",
+  "Estudiante modificado con exito ",
+  "Estudiante eliminado con exito ",
+  "Error en la base de datos "
+
+};
+
+
 Connection * openConnection();
 requestState writeRequest(Request * request, int fd);
 int closeSocket(int fd) ;
